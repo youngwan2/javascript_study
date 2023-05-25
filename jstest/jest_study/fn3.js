104d20bdbf5c02168af08462ba0b45d723c34b29
KYW <qodna25@gmail.com>
2023-05-25 14:14:45 +0900

    js30-01 1. 타이머를 사용한 글자수 출력하기 까지

--- jstest/jest_study/fn3.js
+++ jstest/jest_study/fn3.js
@@ -1,11 +1,24 @@
 
 
 
-function forEach(items,callback){
-    for(let i =0; i<items.length; i++){
-        callback(items[i])
-    }
-}
+// function forEach(items,callback){
+//     for(let i =0; i<items.length; i++){
+//         callback(items[i])
+//     }
+// }
 
 
-module.exports = forEach
+// module.exports = forEach
+
+
+const ret = [1,2,3,4,5,6,888,666]
+
+
+const maxValue =ret.reduce((max,num)=>{
+    console.log("max:",max, "num:",num)
+    return num > max ? num:max
+},0 /* 초깃값이 들어감 */)
+
+
+console.log(maxValue)
+
